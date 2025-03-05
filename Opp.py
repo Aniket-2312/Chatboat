1a8b6e627ff50705952da8454a8cde4782f4e72f
Aniket-2312 <aniketvpawar2003@gmail.com>
2025-03-05 20:39:15 +0530

    Update Opp.py

--- Opp.py
+++ Opp.py
@@ -72,4 +72,62 @@ class Atm:
       print('sale chor')
     self.menu()
 
+obj1 = Atm()
+id(obj1)
+
+
+
+# Class Fraction:
+class Fraction:
+
+  # parameterized constructor
+  def __init__(self,x,y):
+    self.num = x
+    self.den = y
+
+  def __str__(self):
+    return '{}/{}'.format(self.num,self.den)
+
+  def __add__(self,other):
+    new_num = self.num*other.den + other.num*self.den
+    new_den = self.den*other.den
+
+    return '{}/{}'.format(new_num,new_den)
+
+  def __sub__(self,other):
+    new_num = self.num*other.den - other.num*self.den
+    new_den = self.den*other.den
+
+    return '{}/{}'.format(new_num,new_den)
+
+  def __mul__(self,other):
+    new_num = self.num*other.num
+    new_den = self.den*other.den
+
+    return '{}/{}'.format(new_num,new_den)
+
+  def __truediv__(self,other):
+    new_num = self.num*other.den
+    new_den = self.den*other.num
+
+    return '{}/{}'.format(new_num,new_den)
+
+  def convert_to_decimal(self):
+    return self.num/self.den
+
+fr1 = Fraction(3,4)
+fr2 = Fraction(1,2)
+fr1.convert_to_decimal()
+# 3/4("Output will look like this ")
+print(fr1 + fr2)
+print(fr1 - fr2)
+print(fr1 * fr2)
+print(fr1 / fr2)
+# Outputs
+#10/8
+#2/8
+#3/8
+#6/4
+
+
 
